e5b6a69439c726c1d39075fe4ed7c62733e48106
ftyler <ftyler.ctr@kr.af.mil>
2022-03-25 08:54:36 -0400

    Initial commit.

---
+++ src/test/java/com/example/tapdemo/TapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
